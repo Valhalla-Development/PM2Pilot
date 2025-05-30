34e9db94ce60a2a603fb23fbb47f5981b1d9a22e
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-05-30 23:52:14 +0100

    ✨ (routes): refactor layout.svelte to use CSS class instead of inline style for better maintainability and consistency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@ import ThemeToggle from '$lib/components/ThemeToggle.svelte';
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<div style="display: contents">
+<div class="contents">
   <ThemeToggle />
   <slot />
 </div> 
