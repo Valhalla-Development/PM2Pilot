7deb5c12a923bc05f5d66f8ffad597c2eb8ffe20
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-06-02 14:22:42 +0100

    ✨ (vanta.ts): introduce VantaWavesConfig interface

--- src/lib/types/vanta.ts
+++ src/lib/types/vanta.ts
@@ -22,10 +22,32 @@ export interface VantaEffect {
     req?: number;
 }
 
+export interface VantaWavesConfig {
+    el: HTMLElement;
+    mouseControls?: boolean;
+    touchControls?: boolean;
+    gyroControls?: boolean;
+    minHeight?: number;
+    minWidth?: number;
+    scale?: number;
+    scaleMobile?: number;
+    color?: number;
+    shininess?: number;
+    waveHeight?: number;
+    waveSpeed?: number;
+    zoom?: number;
+}
+
 export type VantaWaves = (config: VantaConfig) => VantaEffect;
 
 export interface VantaGlobal {
-    WAVES: VantaWaves;
+    WAVES: (config: VantaWavesConfig) => VantaEffect;
+}
+
+declare global {
+    interface Window {
+        VANTA: VantaGlobal;
+    }
 }
 
 export interface VantaWindow extends Window {
